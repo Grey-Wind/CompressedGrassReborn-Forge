320df422702ef84a39f804b13a55301fd3cf938c
Adenx0 <98391767+Adenx0@users.noreply.github.com>
2022-05-17 21:10:46 +0800

    4.0.0
    add grass medicine

---
+++ src/main/java/net/mcreator/compressedgrass/item/GrassMedicineItem.java
@@ -0,0 +1,73 @@
+
+package net.mcreator.compressedgrass.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.compressedgrass.procedures.GrassMedicineEffectProcedure;
+import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap;
+
+@CompressedGrassModElements.ModElement.Tag
+public class GrassMedicineItem extends CompressedGrassModElements.ModElement {
+	@ObjectHolder("compressed_grass:grass_medicine")
+	public static final Item block = null;
+
+	public GrassMedicineItem(CompressedGrassModElements instance) {
+		super(instance, 169);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f)
+
+							.build()));
+			setRegistryName("grass_medicine");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 32;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public ItemStack onItemUseFinish(ItemStack itemstack, World world, LivingEntity entity) {
+			ItemStack retval = super.onItemUseFinish(itemstack, world, entity);
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+
+			GrassMedicineEffectProcedure.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("entity", entity)).collect(HashMap::new,
+					(_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			return retval;
+		}
+	}
+}
