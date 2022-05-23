628f1d8820087856d7172d6bbfa089885cb645f7
Adenx0 <98391767+Adenx0@users.noreply.github.com>
2022-05-23 13:50:09 +0800

    4.1.0

---
+++ src/main/java/net/mcreator/compressedgrass/item/GrassCakeItem.java
@@ -0,0 +1,65 @@
+
+package net.mcreator.compressedgrass.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.compressedgrass.itemgroup.GrassItemGroup;
+import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+import java.util.List;
+
+@CompressedGrassModElements.ModElement.Tag
+public class GrassCakeItem extends CompressedGrassModElements.ModElement {
+	@ObjectHolder("compressed_grass:grass_cake")
+	public static final Item block = null;
+
+	public GrassCakeItem(CompressedGrassModElements instance) {
+		super(instance, 173);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(GrassItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
+					.food((new Food.Builder()).hunger(4).saturation(0.3f)
+
+							.build()));
+			setRegistryName("grass_cake");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 32;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Thank you\uFF0CAnnijang."));
+		}
+	}
+}
