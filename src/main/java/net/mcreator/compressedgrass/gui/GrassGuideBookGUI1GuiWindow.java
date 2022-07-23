40a52ecdcb95f20e65095cf287094a98b3695707
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-07-23 10:59:33 +0800

    4.6.0

--- src/main/java/net/mcreator/compressedgrass/gui/GrassGuideBookGUI1GuiWindow.java
+++ src/main/java/net/mcreator/compressedgrass/gui/GrassGuideBookGUI1GuiWindow.java
@@ -5,12 +5,16 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.Minecraft;
+
+import net.mcreator.compressedgrass.CompressedGrassMod;
 
 import java.util.HashMap;
 
@@ -77,6 +81,15 @@ public class GrassGuideBookGUI1GuiWindow extends ContainerScreen<GrassGuideBookG
 		this.font.drawString(ms, "made by Adenx updates original", 6, 25, -12829636);
 		this.font.drawString(ms, "1.7.10 version of the mod to", 6, 34, -12829636);
 		this.font.drawString(ms, "1.16.5.", 6, 43, -12829636);
+		this.font.drawString(ms, "I have sent a request to", 24, 70, -12829636);
+		this.font.drawString(ms, "Cooperation", 60, 61, -16777216);
+		this.font.drawString(ms, "NeversXD(author of 1.7.10) on", 6, 79, -12829636);
+		this.font.drawString(ms, "CurseForge to co-produce version", 6, 88, -12829636);
+		this.font.drawString(ms, "1.16.5. I found the user with", 6, 97, -12829636);
+		this.font.drawString(ms, "the same name on Github,but I", 6, 106, -12829636);
+		this.font.drawString(ms, "couldn't check the warehouse,so", 6, 115, -12829636);
+		this.font.drawString(ms, "I didn't send a message to him", 6, 124, -12829636);
+		this.font.drawString(ms, "on Github.", 6, 133, -12829636);
 	}
 
 	@Override
@@ -89,5 +102,11 @@ public class GrassGuideBookGUI1GuiWindow extends ContainerScreen<GrassGuideBookG
 	public void init(Minecraft minecraft, int width, int height) {
 		super.init(minecraft, width, height);
 		minecraft.keyboardListener.enableRepeatEvents(true);
+		this.addButton(new Button(this.guiLeft + 96, this.guiTop + 142, 72, 20, new StringTextComponent("Next Page"), e -> {
+			if (true) {
+				CompressedGrassMod.PACKET_HANDLER.sendToServer(new GrassGuideBookGUI1Gui.ButtonPressedMessage(0, x, y, z));
+				GrassGuideBookGUI1Gui.handleButtonAction(entity, 0, x, y, z);
+			}
+		}));
 	}
 }
---
+++ src/main/java/net/mcreator/compressedgrass/item/OctupleArmorItem.java
@@ -0,0 +1,106 @@
+
+package net.mcreator.compressedgrass.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+@CompressedGrassModElements.ModElement.Tag
+public class OctupleArmorItem extends CompressedGrassModElements.ModElement {
+	@ObjectHolder("compressed_grass:octuple_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("compressed_grass:octuple_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("compressed_grass:octuple_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("compressed_grass:octuple_armor_boots")
+	public static final Item boots = null;
+
+	public OctupleArmorItem(CompressedGrassModElements instance) {
+		super(instance, 216);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 105;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{14, 42, 35, 14}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 63;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(OctupleCompressedGrassItem.block));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "octuple_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 0f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/octuple_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("octuple_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/octuple_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("octuple_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/octuple_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("octuple_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/octuple_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("octuple_armor_boots"));
+	}
+
+}
