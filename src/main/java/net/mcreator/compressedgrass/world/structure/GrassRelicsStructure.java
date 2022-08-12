5363586d7cf06bbc4afef23d284272eb35a860d9
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-08-12 22:06:36 +0800

    5.3.0

--- src/main/java/net/mcreator/compressedgrass/world/structure/GrassRelicsStructure.java
+++ src/main/java/net/mcreator/compressedgrass/world/structure/GrassRelicsStructure.java
@@ -52,7 +52,7 @@ public class GrassRelicsStructure {
 						dimensionCriteria = true;
 					if (!dimensionCriteria)
 						return false;
-					if ((random.nextInt(1000000) + 1) <= 1000) {
+					if ((random.nextInt(1000000) + 1) <= 750) {
 						int count = random.nextInt(1) + 1;
 						for (int a = 0; a < count; a++) {
 							int i = ci + random.nextInt(16);
