2dac3663bd054a950862f25ccfa9b5291ca848b8
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-08-22 20:33:46 +0800

    5.6.0
    add night vision

--- src/main/java/net/mcreator/compressedgrass/item/NonupleGrassArmorItem.java
+++ src/main/java/net/mcreator/compressedgrass/item/NonupleGrassArmorItem.java
@@ -6,6 +6,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
+import net.minecraft.world.World;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.ItemStack;
@@ -14,9 +15,16 @@ import net.minecraft.item.Item;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.Entity;
 
+import net.mcreator.compressedgrass.procedures.NonupleGrassArmorHeadProcedure;
 import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap;
 
 @CompressedGrassModElements.ModElement.Tag
 public class NonupleGrassArmorItem extends CompressedGrassModElements.ModElement {
@@ -81,6 +89,17 @@ public class NonupleGrassArmorItem extends CompressedGrassModElements.ModElement
 			@Override
 			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
 				return "compressed_grass:textures/models/armor/nonuplegrass_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+
+			@Override
+			public void onArmorTick(ItemStack itemstack, World world, PlayerEntity entity) {
+				super.onArmorTick(itemstack, world, entity);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+
+				NonupleGrassArmorHeadProcedure.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("entity", entity)).collect(HashMap::new,
+						(_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 			}
 		}.setRegistryName("nonuple_grass_armor_helmet"));
 		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
