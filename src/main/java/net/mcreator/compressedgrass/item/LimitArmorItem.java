d9392bb967126b066a36834ee2dc20e4745e5557
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-05 18:31:51 +0800

    add limit armor

---
+++ src/main/java/net/mcreator/compressedgrass/item/LimitArmorItem.java
@@ -0,0 +1,106 @@
+
+package net.mcreator.compressedgrass.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IArmorMaterial;
+import net.minecraft.item.ArmorItem;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+@CompressedGrassModElements.ModElement.Tag
+public class LimitArmorItem extends CompressedGrassModElements.ModElement {
+	@ObjectHolder("compressed_grass:limit_armor_helmet")
+	public static final Item helmet = null;
+	@ObjectHolder("compressed_grass:limit_armor_chestplate")
+	public static final Item body = null;
+	@ObjectHolder("compressed_grass:limit_armor_leggings")
+	public static final Item legs = null;
+	@ObjectHolder("compressed_grass:limit_armor_boots")
+	public static final Item boots = null;
+
+	public LimitArmorItem(CompressedGrassModElements instance) {
+		super(instance, 315);
+	}
+
+	@Override
+	public void initElements() {
+		IArmorMaterial armormaterial = new IArmorMaterial() {
+			@Override
+			public int getDurability(EquipmentSlotType slot) {
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 210;
+			}
+
+			@Override
+			public int getDamageReductionAmount(EquipmentSlotType slot) {
+				return new int[]{15, 25, 35, 20}[slot.getIndex()];
+			}
+
+			@Override
+			public int getEnchantability() {
+				return 75;
+			}
+
+			@Override
+			public net.minecraft.util.SoundEvent getSoundEvent() {
+				return (net.minecraft.util.SoundEvent) ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation(""));
+			}
+
+			@Override
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(LimitCompressedGrassItem.block));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public String getName() {
+				return "limit_armor";
+			}
+
+			@Override
+			public float getToughness() {
+				return 6f;
+			}
+
+			@Override
+			public float getKnockbackResistance() {
+				return 0.8f;
+			}
+		};
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.HEAD, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/limit_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("limit_armor_helmet"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.CHEST, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/limit_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("limit_armor_chestplate"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.LEGS, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/limit_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("limit_armor_leggings"));
+		elements.items.add(() -> new ArmorItem(armormaterial, EquipmentSlotType.FEET, new Item.Properties().group(ItemGroup.COMBAT)) {
+			@Override
+			public String getArmorTexture(ItemStack stack, Entity entity, EquipmentSlotType slot, String type) {
+				return "compressed_grass:textures/models/armor/limit_layer_" + (slot == EquipmentSlotType.LEGS ? "2" : "1") + ".png";
+			}
+		}.setRegistryName("limit_armor_boots"));
+	}
+
+}
