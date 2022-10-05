9d6f8e3dd31589a7d146826521d42e85279fb772
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-05 18:22:25 +0800

    add enchanted grass

---
+++ src/main/java/net/mcreator/compressedgrass/item/EnchantedGrassItem.java
@@ -0,0 +1,52 @@
+
+package net.mcreator.compressedgrass.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+@CompressedGrassModElements.ModElement.Tag
+public class EnchantedGrassItem extends CompressedGrassModElements.ModElement {
+	@ObjectHolder("compressed_grass:enchanted_grass")
+	public static final Item block = null;
+
+	public EnchantedGrassItem(CompressedGrassModElements instance) {
+		super(instance, 314);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("enchanted_grass");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+	}
+}
