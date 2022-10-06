6d2a4b0aa535098faee2e5139c71fc17adb56b59
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-10-06 17:19:55 +0800

    enchant hoe

--- src/main/java/net/mcreator/compressedgrass/item/EnchantedHoeItem.java
+++ src/main/java/net/mcreator/compressedgrass/item/EnchantedHoeItem.java
@@ -3,14 +3,22 @@ package net.mcreator.compressedgrass.item;
 
 import net.minecraftforge.registries.ObjectHolder;
 
+import net.minecraft.world.World;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.HoeItem;
+import net.minecraft.entity.Entity;
 
+import net.mcreator.compressedgrass.procedures.EnchantedHoeOnHandProcedure;
 import net.mcreator.compressedgrass.CompressedGrassModElements;
+
+import java.util.stream.Stream;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap;
 
 @CompressedGrassModElements.ModElement.Tag
 public class EnchantedHoeItem extends CompressedGrassModElements.ModElement {
@@ -48,6 +56,17 @@ public class EnchantedHoeItem extends CompressedGrassModElements.ModElement {
 				return Ingredient.fromStacks(new ItemStack(EnchantedGrassItem.block));
 			}
 		}, 0, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			public void inventoryTick(ItemStack itemstack, World world, Entity entity, int slot, boolean selected) {
+				super.inventoryTick(itemstack, world, entity, slot, selected);
+				double x = entity.getPosX();
+				double y = entity.getPosY();
+				double z = entity.getPosZ();
+				if (selected)
+
+					EnchantedHoeOnHandProcedure.executeProcedure(Stream.of(new AbstractMap.SimpleEntry<>("entity", entity)).collect(HashMap::new,
+							(_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
+			}
 		}.setRegistryName("enchanted_hoe"));
 	}
 }
---
+++ src/main/java/net/mcreator/compressedgrass/procedures/EnchantedHoeOnHandProcedure.java
@@ -0,0 +1,33 @@
+package net.mcreator.compressedgrass.procedures;
+
+import net.minecraft.entity.Entity;
+
+import net.mcreator.compressedgrass.CompressedGrassMod;
+
+import java.util.Map;
+
+public class EnchantedHoeOnHandProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				CompressedGrassMod.LOGGER.warn("Failed to load dependency entity for procedure EnchantedHoeOnHand!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent.world.getServer() != null) {
+				_ent.world.getServer().getCommandManager().handleCommand(_ent.getCommandSource().withFeedbackDisabled().withPermissionLevel(4),
+						"enchant @p minecraft:efficiency 3");
+			}
+		}
+		{
+			Entity _ent = entity;
+			if (!_ent.world.isRemote && _ent.world.getServer() != null) {
+				_ent.world.getServer().getCommandManager().handleCommand(_ent.getCommandSource().withFeedbackDisabled().withPermissionLevel(4),
+						"/enchant @p minecraft:fortune 3");
+			}
+		}
+	}
+}
