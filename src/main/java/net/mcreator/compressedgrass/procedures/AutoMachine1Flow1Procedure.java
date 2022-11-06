9a76de77d07be1a86a8677924e5cfa7805b329c5
AT Studio <98391767+Adenx0@users.noreply.github.com>
2022-11-06 16:38:20 +0800

    add flow

---
+++ src/main/java/net/mcreator/compressedgrass/procedures/AutoMachine1Flow1Procedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.compressedgrass.procedures;
+
+import java.util.Map;
+
+public class AutoMachine1Flow1Procedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+
+	}
+}
