f9941723b220ec69ec8fb5d1a73032ee797bff10
Grey-Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-11 19:24:28 +0800

    reduced the duration of Potato Protection Potion

--- src/main/java/net/mcreator/compressedgrass/init/CompressedGrassModPotions.java
+++ src/main/java/net/mcreator/compressedgrass/init/CompressedGrassModPotions.java
@@ -16,7 +16,7 @@ import net.mcreator.compressedgrass.CompressedGrassMod;
 public class CompressedGrassModPotions {
 	public static final DeferredRegister<Potion> REGISTRY = DeferredRegister.create(ForgeRegistries.POTIONS, CompressedGrassMod.MODID);
 	public static final RegistryObject<Potion> POTATO_PROTECTION_POTION = REGISTRY.register("potato_protection_potion",
-			() -> new Potion(new MobEffectInstance(CompressedGrassModMobEffects.POTATO_PROTECTION.get(), 6000, 0, false, true)));
+			() -> new Potion(new MobEffectInstance(CompressedGrassModMobEffects.POTATO_PROTECTION.get(), 2400, 0, false, true)));
 	public static final RegistryObject<Potion> GRASS_FLY_POTION = REGISTRY.register("grass_fly_potion",
 			() -> new Potion(new MobEffectInstance(CompressedGrassModMobEffects.GRASS_FLY_POTION_EFFECT.get(), 6000, 0, false, true)));
 }
