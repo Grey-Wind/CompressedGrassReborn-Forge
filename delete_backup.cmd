ef1693694f2c0f4fa0ff175f8831cf20399bd34a
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-14 13:45:50 +0800

    6.4.0 (#17)
    * summon god item
    
    * summon god 
    
    * build

--- delete_backup.cmd
+++ delete_backup.cmd
@@ -2,3 +2,4 @@
 del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups\*.*
 cd  D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
 rd /s /q   D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
+del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\run\logs\*.gz
