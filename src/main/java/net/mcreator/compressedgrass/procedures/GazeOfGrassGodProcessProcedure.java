2afb218c30415c7b2d6b2647b2774c05480fe1e0
Grey-Wind <98391767+Grey-Wind@users.noreply.github.com>
2022-12-17 21:44:21 +0800

    change process

--- src/main/java/net/mcreator/compressedgrass/procedures/GazeOfGrassGodProcessProcedure.java
+++ src/main/java/net/mcreator/compressedgrass/procedures/GazeOfGrassGodProcessProcedure.java
@@ -1,6 +1,23 @@
 package net.mcreator.compressedgrass.procedures;
 
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
+
+import net.mcreator.compressedgrass.init.CompressedGrassModEnchantments;
+
 public class GazeOfGrassGodProcessProcedure {
-	public static void execute() {
+	public static void execute(Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
+		if (EnchantmentHelper.getItemEnchantmentLevel(CompressedGrassModEnchantments.GAZE_OF_GRASS_GOD.get(), itemstack) == 0) {
+		}
+		if (EnchantmentHelper.getItemEnchantmentLevel(CompressedGrassModEnchantments.GAZE_OF_GRASS_GOD.get(), itemstack) == 1) {
+			if (entity instanceof LivingEntity _entity)
+				_entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 60, 1));
+		}
 	}
 }
