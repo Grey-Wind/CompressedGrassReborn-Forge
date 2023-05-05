6ed1f4cb595b8a59ad8a4b6b1ff7b0cee874f29e
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-05-05 17:56:31 +0800

    重写Forge版本
    参照原版本

--- delete_backup.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups\*.*
-cd  D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
-rd /s /q   D:\Create\mod_project\CompressedGrassReborn-Forge\.mcreator\workspaceBackups
-del /f /s /q D:\Create\mod_project\CompressedGrassReborn-Forge\run\logs\*.gz
