c273deea625b649460b5b6a2bf5ac2d292d3be9b
Grey Wind <98391767+Grey-Wind@users.noreply.github.com>
2023-07-27 16:48:58 +0800

    creative tab and bug fix

--- src/main/java/net/mcreator/compressedgrass/init/CompressedGrassModTabs.java
+++ src/main/java/net/mcreator/compressedgrass/init/CompressedGrassModTabs.java
@@ -8,17 +8,30 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.CreativeModeTabEvent;
 
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.chat.Component;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CompressedGrassModTabs {
 	@SubscribeEvent
 	public static void buildTabContentsVanilla(CreativeModeTabEvent.BuildContents tabData) {
 
-		if (tabData.getTab() == CreativeModeTabs.NATURAL_BLOCKS) {
-			tabData.accept(CompressedGrassModItems.PRIMARY_COMPRESSED_GRASS.get());
-			tabData.accept(CompressedGrassModItems.SECONDARY_COMPRESSED_GRASS.get());
-			tabData.accept(CompressedGrassModItems.TERTIARY_COMPRESSED_GRASS.get());
+		if (tabData.getTab() == CreativeModeTabs.INGREDIENTS) {
+			tabData.accept(CompressedGrassModItems.GRASS_ORE_INGOT.get());
 		}
+	}
+
+	@SubscribeEvent
+	public static void buildTabContentsModded(CreativeModeTabEvent.Register event) {
+		event.registerCreativeModeTab(new ResourceLocation("compressed_grass", "compressed_grass"),
+				builder -> builder.title(Component.translatable("item_group.compressed_grass.compressed_grass")).icon(() -> new ItemStack(Blocks.GRASS)).displayItems((parameters, tabData) -> {
+					tabData.accept(CompressedGrassModItems.PRIMARY_COMPRESSED_GRASS.get());
+					tabData.accept(CompressedGrassModItems.SECONDARY_COMPRESSED_GRASS.get());
+					tabData.accept(CompressedGrassModItems.TERTIARY_COMPRESSED_GRASS.get());
+					tabData.accept(CompressedGrassModItems.QUATERNARY_COMPRESSED_GRASS.get());
+				}).withSearchBar());
 	}
 }
